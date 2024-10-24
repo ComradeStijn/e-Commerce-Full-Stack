a65b880df68394ce453f812f539bd1323fb3cc42
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-24 22:07:33 +0200

    feat:

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -1,1 +1,21 @@
-import './home.scss';
+import "./home.scss";
+
+const navBar = document.querySelector("#nav");
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+    } else {
+      entry.target.classlist.remove("is-visible");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, {
+  root: null,
+  rootMargin: '0px',
+  treshold: 0.1
+})
+
+observer.observe(navBar);
