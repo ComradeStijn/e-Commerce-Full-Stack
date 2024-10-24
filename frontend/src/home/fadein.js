4b54ce5fb066827e059a4b215074bd07725363ad
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-25 00:09:58 +0200

    fix: Fade-in

--- frontend/src/home/fadein.js
+++ frontend/src/home/fadein.js
@@ -3,11 +3,24 @@ const heroImage = document.querySelector("#hero-img-container");
 const heroTitle = document.querySelector(".hero-main-title");
 
 function addClassOnLoad(elements, className) {
-  window.addEventListener("load", () => {
-    elements.forEach(element => {
-      element.classList.add(className);
-    })
-  })
+  window.addEventListener('load', () => {
+    // Prevent animations from playing whilst webpage is being loaded in background and not visible
+    if (document.visibilityState === 'visible') {
+      elements.forEach(element => {
+        element.classList.add(className);
+      });
+    } else {
+      // Listen for visibility change and trigger animations when the page becomes visible
+      document.addEventListener('visibilitychange', function onVisibilityChange() {
+        if (document.visibilityState === 'visible') {
+          elements.forEach(element => {
+            element.classList.add(className);
+          });
+          document.removeEventListener('visibilitychange', onVisibilityChange); // Clean up listener
+        }
+      });
+    }
+  });
 }
 
 addClassOnLoad([navBar, heroImage, heroTitle], 'is-visible');
