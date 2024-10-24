4dd4abfedfc54042941615af82588deca03edc47
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-24 22:51:10 +0200

    fix: Move fadein effect to dedicated js file

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -1,21 +1,2 @@
 import "./home.scss";
-
-const navBar = document.querySelector("#nav");
-
-const callback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("is-visible");
-    } else {
-      entry.target.classlist.remove("is-visible");
-    }
-  });
-};
-
-const observer = new IntersectionObserver(callback, {
-  root: null,
-  rootMargin: '0px',
-  treshold: 0.1
-})
-
-observer.observe(navBar);
+import "./fadein.js";
