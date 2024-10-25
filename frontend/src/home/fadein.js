df0a4cfbdb2758ecd499385278140ff945a38f3e
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-25 15:04:15 +0200

    feat: Product category fade-in in home.html

--- frontend/src/home/fadein.js
+++ frontend/src/home/fadein.js
@@ -3,24 +3,49 @@ const heroImage = document.querySelector("#hero-img-container");
 const heroTitle = document.querySelector(".hero-main-title");
 
 function addClassOnLoad(elements, className) {
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     // Prevent animations from playing whilst webpage is being loaded in background and not visible
-    if (document.visibilityState === 'visible') {
-      elements.forEach(element => {
+    if (document.visibilityState === "visible") {
+      elements.forEach((element) => {
         element.classList.add(className);
       });
     } else {
       // Listen for visibility change and trigger animations when the page becomes visible
-      document.addEventListener('visibilitychange', function onVisibilityChange() {
-        if (document.visibilityState === 'visible') {
-          elements.forEach(element => {
-            element.classList.add(className);
-          });
-          document.removeEventListener('visibilitychange', onVisibilityChange); // Clean up listener
+      document.addEventListener(
+        "visibilitychange",
+        function onVisibilityChange() {
+          if (document.visibilityState === "visible") {
+            elements.forEach((element) => {
+              element.classList.add(className);
+            });
+            document.removeEventListener(
+              "visibilitychange",
+              onVisibilityChange
+            ); // Clean up listener
+          }
         }
-      });
+      );
     }
   });
 }
 
-addClassOnLoad([navBar, heroImage, heroTitle], 'is-visible');
+addClassOnLoad([navBar, heroImage, heroTitle], "is-visible");
+
+const productElements = document.querySelectorAll(".product");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+    root: null,
+    rootMargin: '0px',
+  }
+);
+
+productElements.forEach(element => observer.observe(element));
