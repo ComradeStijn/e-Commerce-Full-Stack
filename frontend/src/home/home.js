b2667e618236833614754d8337d4cb25bc7ade4e
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-25 14:27:25 +0200

    feat: Product categories on home.html

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -4,3 +4,14 @@ import "./fadein.js";
 import heroImgURL from '../assets/hero.jpg';
 
 document.querySelector('#hero-img').src = heroImgURL;
+
+import phoneImgUrl from '../assets/phone.jpg';
+import tabletImgUrl from '../assets/tablet.jpg';
+import pcImgUrl from '../assets/pc.jpg';
+import watchImgUrl from '../assets/smartwatch.jpg';
+
+document.querySelector('#product-phone img').src = phoneImgUrl;
+document.querySelector('#product-tablet img').src = tabletImgUrl;
+document.querySelector('#product-pc img').src = pcImgUrl;
+document.querySelector('#product-smartwatch img').src = watchImgUrl;
+
